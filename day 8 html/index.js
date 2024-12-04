8eacb3d9f522eec5cb2eef91f5f1997cf24d867e
ayush2044 <ayushpratap2044@gmail.com>
2024-12-04 15:53:59 +0530

    day 8

---
+++ day 8 html/index.js
@@ -0,0 +1,47 @@
+// let arr = [1,12,21,21,'horse'];
+// for(let i = 0; i <= arr.length; i++){
+//      console.log(arr[i]);
+// }
+// // let ar = [5,5,6,6,6,5,4,56,7,3];
+// //     let count = 0;
+// // for (let i = 0; i < ar.length;i++){
+// //     // console.log(ar[i]);
+// //     if(ar[i]%2 === 0){
+// //         console.log(ar[i]);
+// //     } 
+// }  
+//  console.log(count);
+let arr = [1,2,3,4,5,7,8,9];
+let count = 0;
+for(let i= 1; i <= arr.length;i++){
+ if(arr[i] !== arr ) {
+
+   console.log();
+ }
+} 
+let ar = [1,2,3,4,5,6,7,8];
+for(let i = 0; i<= arr.length; i++){
+    
+}
+let student_id_card = {
+    name: "rohar",
+    class1: '12th',
+    address: " chandramukhi haveli",
+    rollno: 34343,
+    section : "c",
+};
+console.log(student_id_card);
+const name = student_id_card.name;
+const class1 = student_id_card.class1;
+const address = student_id_card.address;
+const rollno = student_id_card.rollno;
+const section = student_id_card.section;
+
+console.log(name);
+console.log(class1);
+console.log(address);
+console.log(rollno);
+console.log(section);
+student_id_card.result = "pass";
+console.log(student_id_card);
+student_id_card.section = "A";
